4ff76aa47685aa201f46f10805a946ad4c906f29
camilo alarcon <edgalarcon@uniboyaca.edu.co>
2026-02-25 13:58:51 -0500

    Cambios esteticos y se agrega la funcion de filtros

--- src/main.js
+++ src/main.js
@@ -5,25 +5,107 @@ import { showPokemon } from "./ui/ui.js";
 // Iniciamos en Pikachu (#25)
 let current = 25;
 
+let filteredIds = null; // null = sin filtro activo
+let filterIndex = 0;
+
 // Carga y muestra un Pokémon por su ID
 async function loadPokemon(id) {
   const pokemon = await fetchPokemon(id);
   showPokemon(pokemon);
+  return pokemon;
 }
 
 // Carga inicial
 loadPokemon(current);
 
+// ===== FILTROS =====
+const checkboxes = document.querySelectorAll(".filter-types input");
+const clearBtn = document.getElementById("clear-filters");
+
+checkboxes.forEach(cb => {
+  cb.addEventListener("change", applyFilters);
+});
+
+clearBtn.addEventListener("click", () => {
+  checkboxes.forEach(cb => cb.checked = false);
+  filteredIds = null;
+  loadPokemon(current);
+});
+
+async function applyFilters() {
+
+  const selectedTypes = Array.from(checkboxes)
+    .filter(cb => cb.checked)
+    .map(cb => cb.value);
+
+  // Si no hay filtros activos
+  if (selectedTypes.length === 0) {
+    filteredIds = null;
+    loadPokemon(current);
+    return;
+  }
+
+  filteredIds = [];
+
+  // Revisar primeros 151 Pokémon
+  for (let i = 1; i <= 151; i++) {
+    const pokemon = await fetchPokemon(i);
+
+    const matches = pokemon.types.some(type =>
+      selectedTypes.includes(type)
+    );
+
+    if (matches) {
+      filteredIds.push(i);
+    }
+  }
+
+  if (filteredIds.length === 0) {
+    alert("No hay Pokémon con esos tipos");
+    return;
+  }
+
+  filterIndex = 0;
+  loadPokemon(filteredIds[filterIndex]);
+}
+
+const toggleBtn = document.getElementById("toggle-filters");
+const filtersContainer = document.querySelector(".filters");
+
+toggleBtn.addEventListener("click", () => {
+  filtersContainer.classList.toggle("hidden-filters");
+});
+
 // Botón siguiente: avanza al siguiente Pokémon
 document.querySelector(".next").addEventListener("click", () => {
-  current++;
-  loadPokemon(current);
+
+  if (filteredIds) {
+    filterIndex++;
+    if (filterIndex >= filteredIds.length) {
+      filterIndex = 0;
+    }
+    loadPokemon(filteredIds[filterIndex]);
+  } else {
+    current++;
+    loadPokemon(current);
+  }
+
 });
 
 // Botón anterior: retrocede (mínimo ID 1)
 document.querySelector(".prev").addEventListener("click", () => {
-  if (current > 1) current--;
-  loadPokemon(current);
+
+  if (filteredIds) {
+    filterIndex--;
+    if (filterIndex < 0) {
+      filterIndex = filteredIds.length - 1;
+    }
+    loadPokemon(filteredIds[filterIndex]);
+  } else {
+    if (current > 1) current--;
+    loadPokemon(current);
+  }
+
 });
 
 // Cerrar el modal al hacer clic en la X
